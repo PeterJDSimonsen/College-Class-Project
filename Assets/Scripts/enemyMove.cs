ec0d438787580bc43a9c856813ce7bc933a1f71a
PeterJDSimonsen <513761@chrome.escg.ac.uk>
2020-10-12 10:20:15 +0100

    Enemy Created

---
+++ Assets/Scripts/enemyMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMove : MonoBehaviour
+{
+    int timer;
+    Rigidbody2D rb;
+    void Start()
+    {
+        timer = 0;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer++;
+        if (timer == 600)
+        {
+            timer = 0;
+            rb.velocity = new Vector2(-5, 0);
+            Debug.Log(timer);
+        }
+        else if (timer == 300)
+        {
+            rb.velocity = new Vector2(5, 0);
+        }
+    }
+}
